5e6e59afcb51522e94f09c3dbf952dbf14bd5682
Etienne Fourie <info@etiennefourie.com>
2022-09-12 16:16:25 +0200

    lazy loading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import 'lazysizes'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 /*import MobileMenu from './modules/MobileMenu'*/
